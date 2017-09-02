f35c9e1568747fe6fcb14eedabb349e706a5e61a
Love F <lovef.code@gmail.com>
2017-09-02 10:18:45 +0200

    Pairwise assert

---
+++ kotlin-assert-utils/src/main/kotlin/se/lovef/assert/iterableAssertUtil.kt
@@ -0,0 +1,46 @@
+package se.lovef.assert
+
+import junit.framework.AssertionFailedError
+import org.junit.Assert
+
+/*
+ * Date: 2017-09-02
+ * @author Love
+ */
+
+/** Executes [assertionBlock] on all elements from [a] and [b] in pairs.
+ * If only one of [a] and [b] is null then [AssertionFailedError]
+ *
+ * @throws AssertionError
+ *      if only one of [a] and [b] are null,
+ *      or if [a] and [b] have different number of elements,
+ *      or if [assertionBlock] fails for any element pair. */
+fun <E> assertPairwise(a: Iterable<E>?, b: Iterable<E>?, assertionBlock: (E, E) -> Unit) {
+    if (a == null && b == null) {
+        return
+    }
+    if (a == null || b == null) {
+        throw AssertionError("$a and $b are both null or both not null")
+    }
+    if (a is Collection<E> && b is Collection<E>) {
+        "a.size == ${a.size} == b.size == ${b.size}" proof {
+            a.size isEqualTo  b.size
+        }
+    }
+    val iteratorA = a.iterator()
+    val iteratorB = b.iterator()
+    while (iteratorA.hasNext()) {
+        val elementA = iteratorA.next()
+        if(!iteratorB.hasNext())
+            Assert.fail("to few elements in $b")
+        val elementB = iteratorB.next()
+        try {
+            assertionBlock(elementA, elementB)
+        } catch (t: Throwable) {
+            throw AssertionError("Assertion of $elementA and $elementB failed", t)
+        }
+    }
+    if (iteratorB.hasNext()) {
+        Assert.fail("to many elements in $b")
+    }
+}
