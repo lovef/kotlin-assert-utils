a5b9f3a65d2b1359040e5530dc657280f08be6ab
Love F <lovef.code@gmail.com>
2017-11-25 20:50:51 +0100

    fix #15 Compare nullable types

--- kotlin-assert-utils/src/main/kotlin/se/lovef/assert/comparableAssertUtil.kt
+++ kotlin-assert-utils/src/main/kotlin/se/lovef/assert/comparableAssertUtil.kt
@@ -7,22 +7,22 @@ import org.junit.Assert
  * @author Love
  */
 
-infix fun <T : Comparable<T>> T.isLessThan(other: T): T {
-    Assert.assertTrue("$this < $other", this < other)
-    return other
+infix fun <T : Comparable<T>> T?.isLessThan(other: T?): T {
+    Assert.assertTrue("$this < $other", this.isNotNull() < other.isNotNull())
+    return other!!
 }
 
-infix fun <T : Comparable<T>> T.isLessOrEqualTo(other: T): T {
-    Assert.assertTrue("$this <= $other", this <= other)
-    return other
+infix fun <T : Comparable<T>> T?.isLessOrEqualTo(other: T?): T {
+    Assert.assertTrue("$this <= $other", this.isNotNull() <= other.isNotNull())
+    return other!!
 }
 
-infix fun <T : Comparable<T>> T.isGreaterThan(other: T): T {
-    Assert.assertTrue("$this > $other", this > other)
-    return other
+infix fun <T : Comparable<T>> T?.isGreaterThan(other: T?): T {
+    Assert.assertTrue("$this > $other", this.isNotNull() > other.isNotNull())
+    return other!!
 }
 
-infix fun <T : Comparable<T>> T.isGreaterOrEqualTo(other: T): T {
-    Assert.assertTrue("$this >= $other", this >= other)
-    return other
+infix fun <T : Comparable<T>> T?.isGreaterOrEqualTo(other: T?): T {
+    Assert.assertTrue("$this >= $other", this.isNotNull() >= other.isNotNull())
+    return other!!
 }
--- kotlin-assert-utils/src/test/kotlin/se/lovef/assert/ComparableAssertUtilKtTest.kt
+++ kotlin-assert-utils/src/test/kotlin/se/lovef/assert/ComparableAssertUtilKtTest.kt
@@ -9,12 +9,14 @@ import java.util.*
  */
 class ComparableAssertUtilKtTest {
 
-    private val OBJECT_A = Date(0)
-    private val OBJECT_B = Date(0)
+    private val OBJECT_0_A: Date? = Date(0)
+    private val OBJECT_0_B: Date? = Date(0)
+    private val OBJECT_1: Date? = Date(0)
 
     @Test fun `is less than`() {
         1 isLessThan 2
         { 1 isLessThan 1 } throws Error::class
+        (OBJECT_0_A isLessOrEqualTo OBJECT_1) referenceIsEqualTo OBJECT_1
         (1 isLessThan 2) isEqualTo 2
         1 isLessThan 2 isLessThan 3
         { 1 isLessThan 1 isLessThan 3 } throws Error::class
@@ -25,7 +27,7 @@ class ComparableAssertUtilKtTest {
         1 isLessOrEqualTo 1
         1 isLessOrEqualTo 2
         { 1 isLessOrEqualTo 0 } throws Error::class
-        (OBJECT_A isLessOrEqualTo OBJECT_B) referenceIsEqualTo OBJECT_B
+        (OBJECT_0_A isLessOrEqualTo OBJECT_0_B) referenceIsEqualTo OBJECT_0_B
         1 isLessOrEqualTo 1 isLessOrEqualTo 1
         { 1 isLessOrEqualTo 0 isLessOrEqualTo 0 } throws Error::class
         { 1 isLessOrEqualTo 1 isLessOrEqualTo 0 } throws Error::class
@@ -34,6 +36,7 @@ class ComparableAssertUtilKtTest {
     @Test fun `is greater than`() {
         2 isGreaterThan 1
         { 1 isGreaterThan 1 } throws Error::class
+        (OBJECT_1 isLessOrEqualTo OBJECT_0_A) referenceIsEqualTo OBJECT_0_A
         (2 isGreaterThan 1) isEqualTo 1
         3 isGreaterThan 2 isGreaterThan 1
         { 3 isGreaterThan 3 isGreaterThan 1 } throws Error::class
@@ -44,9 +47,30 @@ class ComparableAssertUtilKtTest {
         1 isGreaterOrEqualTo 1
         2 isGreaterOrEqualTo 1
         { 1 isGreaterOrEqualTo 2 } throws Error::class
-        (OBJECT_A isGreaterOrEqualTo OBJECT_B) referenceIsEqualTo OBJECT_B
+        (OBJECT_0_A isGreaterOrEqualTo OBJECT_0_B) referenceIsEqualTo OBJECT_0_B
         1 isGreaterOrEqualTo 1 isGreaterOrEqualTo 1
         { 1 isGreaterOrEqualTo 2 isGreaterOrEqualTo 2 } throws Error::class
         { 1 isGreaterOrEqualTo 1 isGreaterOrEqualTo 2 } throws Error::class
+    }
+
+    @Test fun `comparing null yields assertion error`() {
+        val nullValue: Int? = null
+        val a: Int? = 0
+
+        { nullValue isLessThan a } throws Error::class
+        { nullValue isLessThan nullValue } throws Error::class
+        { a isLessThan nullValue } throws Error::class
+
+        { nullValue isLessOrEqualTo a } throws Error::class
+        { nullValue isLessOrEqualTo nullValue } throws Error::class
+        { a isLessOrEqualTo nullValue } throws Error::class
+
+        { nullValue isGreaterThan a } throws Error::class
+        { nullValue isGreaterThan nullValue } throws Error::class
+        { a isGreaterThan nullValue } throws Error::class
+
+        { nullValue isGreaterOrEqualTo a } throws Error::class
+        { nullValue isGreaterOrEqualTo nullValue } throws Error::class
+        { a isGreaterOrEqualTo nullValue } throws Error::class
     }
 }
