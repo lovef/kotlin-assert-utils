71c78116199cc259726fffe5b0df51f5e93107fd
Love F <lovef.code@gmail.com>
2018-03-21 18:43:28 +0100

    rel #26 "value 123" doesContain 123

--- kotlin-assert-utils/src/main/kotlin/se/lovef/assert/textAssertUtil.kt
+++ kotlin-assert-utils/src/main/kotlin/se/lovef/assert/textAssertUtil.kt
@@ -8,12 +8,15 @@ import org.intellij.lang.annotations.Language
  * @author Love
  */
 
-infix fun CharSequence?.doesContain(other: CharSequence) = isNotNull().apply {
-    MatcherAssert.assertThat("'$this' does contain '$other'", this.contains(other))
+infix fun CharSequence?.doesContain(other: Any?) = isNotNull().apply {
+    MatcherAssert.assertThat("'$this' does contain '$other'", this.contains(other as? CharSequence ?: "$other"))
 }
 
-infix fun CharSequence?.doesNotContain(other: CharSequence) = apply {
-    MatcherAssert.assertThat("'$this' does not contain '$other'", this == null || !this.contains(other))
+infix fun CharSequence?.doesNotContain(other: Any?) = apply {
+    MatcherAssert.assertThat(
+        "'$this' does not contain '$other'",
+        this == null || !this.contains(other as? CharSequence ?: "$other")
+    )
 }
 
 infix fun CharSequence?.doesMatch(@Language("RegExp") regex: CharSequence) = isNotNull().apply {
@@ -25,25 +28,34 @@ infix fun CharSequence?.doesMatch(regex: Regex) = isNotNull().apply {
 }
 
 infix fun CharSequence?.doesNotMatch(@Language("RegExp") regex: CharSequence) = apply {
-    MatcherAssert.assertThat("'$this' does not match '$regex'", this == null || !this.contains(regex.toString().toRegex()))
+    MatcherAssert.assertThat(
+        "'$this' does not match '$regex'",
+        this == null || !this.contains(regex.toString().toRegex())
+    )
 }
 
 infix fun CharSequence?.doesNotMatch(@Language("RegExp") regex: Regex) = apply {
     MatcherAssert.assertThat("'$this' does not match '$regex'", this == null || !this.contains(regex))
 }
 
-infix fun CharSequence?.doesStartWith(start: CharSequence)  = isNotNull().apply {
-    MatcherAssert.assertThat("'$this' does start with '$start'", this.startsWith(start))
+infix fun CharSequence?.doesStartWith(start: Any?) = isNotNull().apply {
+    MatcherAssert.assertThat("'$this' does start with '$start'", this.startsWith(start as? CharSequence ?: "$start"))
 }
 
-infix fun CharSequence?.doesNotStartWith(start: CharSequence)  = apply {
-    MatcherAssert.assertThat("'$this' does not start with '$start'", this == null || !this.startsWith(start))
+infix fun CharSequence?.doesNotStartWith(start: Any?) = apply {
+    MatcherAssert.assertThat(
+        "'$this' does not start with '$start'",
+        this == null || !this.startsWith(start as? CharSequence ?: "$start")
+    )
 }
 
-infix fun CharSequence?.doesEndWith(end: CharSequence)  = isNotNull().apply {
-    MatcherAssert.assertThat("'$this' does end with '$end'", this.endsWith(end))
+infix fun CharSequence?.doesEndWith(end: Any?) = isNotNull().apply {
+    MatcherAssert.assertThat("'$this' does end with '$end'", this.endsWith(end as? CharSequence ?: "$end"))
 }
 
-infix fun CharSequence?.doesNotEndWith(end: CharSequence)  = apply {
-    MatcherAssert.assertThat("'$this' does not end with '$end'", this == null || !this.endsWith(end))
+infix fun CharSequence?.doesNotEndWith(end: Any?) = apply {
+    MatcherAssert.assertThat(
+        "'$this' does not end with '$end'",
+        this == null || !this.endsWith(end as? CharSequence ?: "$end")
+    )
 }
--- kotlin-assert-utils/src/test/kotlin/se/lovef/assert/TextAssertUtilTest.kt
+++ kotlin-assert-utils/src/test/kotlin/se/lovef/assert/TextAssertUtilTest.kt
@@ -13,14 +13,20 @@ class TextAssertUtilTest {
         { "my awesome string" doesContain "shit" } throws Error::class
         { "my awesome string" doesContain "we.+me" } throws Error::class
 
-        "my awesome string" doesContain "my" doesContain "awesome" doesContain "string"
+        "my 1337 null string" doesContain 1337 doesContain null
+        { "my 1337 null string" doesContain 666 } throws Error::class
+        { "my 1337 string" doesContain null } throws Error::class
+
+        "my 1337 string" doesContain "my" doesContain 1337 doesContain "string"
 
         { null doesContain "shit" } throws Error::class
     }
 
     @Test fun `does not contain`() {
         "my awesome string" as CharSequence doesNotContain "shit" as CharSequence referenceIsEqualTo "my awesome string"
+        "other string" doesNotContain 1337 doesNotContain null
         { "my awesome string" doesNotContain "awesome" } throws Error::class
+        { "my 1337 string" doesNotContain 1337 } throws Error::class
 
         null doesNotContain "awesome"
     }
@@ -50,12 +56,24 @@ class TextAssertUtilTest {
         "my awesome string" as CharSequence doesStartWith "my" as CharSequence referenceIsEqualTo "my awesome string"
         { "my awesome string" doesStartWith "awesome" } throws Error::class
 
+        "1337 string" doesStartWith 1337
+        { "string 1337" doesStartWith 1337 } throws Error::class
+
+        "null string" doesStartWith null
+        { "string null" doesStartWith null } throws Error::class
+
         { null doesStartWith "shit" } throws Error::class
     }
 
     @Test fun `does not start with`() {
         "my awesome string" as CharSequence doesNotStartWith "awesome" as CharSequence referenceIsEqualTo "my awesome string"
         { "my awesome string" doesNotStartWith "my" } throws Error::class
+
+        "string 1337" doesNotStartWith 1337
+        { "1337 string" doesNotStartWith 1337 } throws Error::class
+
+        "string null" doesNotStartWith null
+        { "null string" doesNotStartWith null } throws Error::class
 
         null doesNotStartWith "shit"
     }
@@ -64,12 +82,24 @@ class TextAssertUtilTest {
         "my awesome string" as CharSequence doesEndWith "string" as CharSequence referenceIsEqualTo "my awesome string"
         { "my awesome string" doesEndWith "awesome" } throws Error::class
 
+        "string 1337" doesEndWith 1337
+        { "1337 string" doesEndWith 1337 } throws Error::class
+
+        "string null" doesEndWith null
+        { "null string" doesEndWith null } throws Error::class
+
         { null doesEndWith "shit" } throws Error::class
     }
 
     @Test fun `does not end with`() {
         "my awesome string" as CharSequence doesNotEndWith "awesome" as CharSequence referenceIsEqualTo "my awesome string"
         { "my awesome string" doesNotEndWith "string" } throws Error::class
+
+        "1337 string" doesNotEndWith 1337
+        { "string 1337" doesNotEndWith 1337 } throws Error::class
+
+        "null string" doesNotEndWith null
+        { "string null" doesNotEndWith null } throws Error::class
 
         null doesNotEndWith "shit"
     }
