06c845e777f823f898bcb0da2edf42d8338876be
Love F <lovef.code@gmail.com>
2018-03-21 19:14:27 +0100

    example 0.3

--- example/src/test/kotlin/se/lovef/example/ExampleTest.kt
+++ example/src/test/kotlin/se/lovef/example/ExampleTest.kt
@@ -18,15 +18,20 @@ class ExampleTest {
         1 isLessThan 2 isLessThan 3
         1 isCloseTo 1.1 tolerance 0.2
 
-        "my awesome string" doesContain "my" doesContain "awesome" doesContain "string"
-        "my awesome string" doesNotContain "shit"
+        "my 1337 string" doesContain "my" doesContain 1337 doesContain "string"
+        "my 1337 string" doesNotContain "shit"
 
         null.isNull()
         ("123" as String?).isNotNull().length isEqualTo 3
 
+        val exception = Exception()
+        ; { throw exception } throws exception
+        { { throw exception } throws Exception() } throws exception
+        { {  } throws exception } throws NotThrownError::class
+
         { "1 is 2" proof { 1 isEqualTo 2 } }
-                .throws(Error::class)
-                .message isEqualTo "1 is 2"
+            .throws(Error::class)
+            .message isEqualTo "1 is 2"
 
         true.isTrue();
         { null.isTrue() } throws Error::class
