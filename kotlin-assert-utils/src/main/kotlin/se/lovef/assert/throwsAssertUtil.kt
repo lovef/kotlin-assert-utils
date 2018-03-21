6617c1b743cf64d1dfdfd5ebb96d73454d687415
Love F <lovef.code@gmail.com>
2018-03-21 17:24:51 +0100

    rel #27 throws exception instance

--- kotlin-assert-utils/src/main/kotlin/se/lovef/assert/throwsAssertUtil.kt
+++ kotlin-assert-utils/src/main/kotlin/se/lovef/assert/throwsAssertUtil.kt
@@ -24,5 +24,21 @@ inline infix fun <reified T : Throwable> (() -> Any?).throws(throwableType: KCla
     throw NotThrownError("${throwableType.java.simpleName} was never thrown")
 }
 
+/** `{ foo() } throws exception` asserts that invoking `foo()` throws the throwable instance `exception`
+ *
+ * Rethrows unexpectedly thrown throwable. Throws [NotThrownError] if nothing was thrown. */
+inline infix fun <reified T : Throwable> (() -> Any?).throws(expected: T): T {
+    try {
+        invoke()
+    } catch (thrown: Throwable) {
+        if (thrown === expected) {
+            return thrown
+        } else {
+            throw thrown
+        }
+    }
+    throw NotThrownError("instance $expected was never thrown")
+}
+
 /** Indicates that expected throwable was never thrown */
 class NotThrownError(message: String) : AssertionFailedError(message)
--- kotlin-assert-utils/src/test/kotlin/se/lovef/assert/ThrowsAssertUtilKtTest.kt
+++ kotlin-assert-utils/src/test/kotlin/se/lovef/assert/ThrowsAssertUtilKtTest.kt
@@ -23,7 +23,7 @@ class ThrowsAssertUtilKtTest {
     class SpecialException(val specialValue: String = "special value") : Exception()
     class OtherException : Exception()
 
-    @Test fun `throws`() {
+    @Test fun `throws type`() {
         val exception = SpecialException();
         { throw exception } throws SpecialException::class referenceIsEqualTo exception
         { throw exception }.throws(SpecialException::class).specialValue isEqualTo exception.specialValue
@@ -37,7 +37,32 @@ class ThrowsAssertUtilKtTest {
     @Test fun `throws throw throwable if type is wrong`() {
         val thrown = SpecialException();
         { { throw thrown } throws OtherException::class }
-                .throws(SpecialException::class)
-                .isEqualTo(thrown)
+            .throws(SpecialException::class)
+            .isEqualTo(thrown)
+    }
+
+    private fun throwException(exception: Exception) {
+        throw exception
+    }
+
+    @Test fun `throws instance`() {
+        val exception = Exception()
+        ;{ throwException(exception) }
+            .throws(exception)
+            .referenceIsEqualTo(exception)
+    }
+
+    @Test fun `throws wrong instance`() {
+        val thrownException = Exception()
+        val expectedException = Exception()
+        ; { { throwException(thrownException) } throws expectedException }
+            .throws(Exception::class)
+            .referenceIsEqualTo(thrownException)
+    }
+
+    @Test fun `throws instance throws exception if nothing is thrown`() {
+        {
+            { } throws Exception()
+        } throws NotThrownError::class
     }
 }
