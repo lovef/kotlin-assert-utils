d3236587a1dde3964adb38da284eec02beaed08a
Love F <lovef.code@gmail.com>
2019-12-08 21:22:53 +0100

    String literal \r and nbsp

--- kotlin-assert-utils/src/main/kotlin/se/lovef/assert/v1/StringUtil.kt
+++ kotlin-assert-utils/src/main/kotlin/se/lovef/assert/v1/StringUtil.kt
@@ -6,6 +6,8 @@ fun String.toStringLiteral() = this
     .replace("\\", "\\\\")
     .replace("\"", "\\\"")
     .replace("\n", "\\n")
+    .replace("\r", "\\r")
     .replace("\t", "\\t")
     .replace("\$", "\\\$")
+    .replace("\u00a0", "\\u00a0")
     .let { "\"$it\"" }
